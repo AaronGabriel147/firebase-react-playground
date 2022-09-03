d7ddad69f0156c170f61e3893990841a426d8c54
AaronGabriel147 <77466761+AaronGabriel147@users.noreply.github.com>
2022-09-03 15:55:05 -0700

    buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+//  This allows for line breaks in textarea. 
+// <p dangerouslySetInnerHTML={{ __html: quote.text }}></p>
+
 import './App.css';
 import { db } from "./firebase.config";
 import { useState, useEffect } from "react";
-import { collection, onSnapshot, addDoc, deleteDoc, doc } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  ref
+} from "firebase/firestore";
 
 
 export default function App() {
@@ -10,9 +20,12 @@ export default function App() {
     title: "",
     text: "",
   });
+  // const [imageUpload, setImageUpload] = useState(null);
+
 
   const quotesCollectionRef = collection(db, "quotes");
 
+  // Read
   useEffect(() => {
     onSnapshot(quotesCollectionRef, (snapshot) => {
       setQuotes(snapshot.docs.map(doc => {
@@ -24,10 +37,11 @@ export default function App() {
     })
   }, [])
 
+  // Create
   const handleSubmit = e => {
     e.preventDefault()
     if (!form.title) {
-      alert("Please fill out all fields")
+      alert("Title cannot be blank.")
       return
     }
 
@@ -38,16 +52,43 @@ export default function App() {
     })
   }
 
-  const removeQuote = id => {
-    deleteDoc(doc(db, "quotes", id))
-  }
+  // Delete
+  const removeQuote = id => deleteDoc(doc(db, "quotes", id))
+
+
+  // ----------------------------------------
+
+
+  // const uploadImage = e => {
+  //   if (imageUpload == null) {
+  //     alert("Please select an image.")
+  //     return
+  //   }
+
+  //   const imageRef = ref(db, `images/${imageUpload.name}`)
+
+  // }
 
   return (
     <div className="app">
 
       {/* FORM */}
 
+
       <form onSubmit={handleSubmit}>
+
+
+        {/* <input
+          id="choose-file"
+          type="file"
+          onChange={e => setImageUpload(e.target.files[0])}
+        />
+        <button onClick={uploadImage}>Upload</button> */}
+
+        {/* {imageUrls.map((url) => {
+          return <img src={url} />;
+        })} */}
+
         <div>
           <label>Title</label>
           <input
@@ -56,19 +97,18 @@ export default function App() {
             onChange={e => setForm({ ...form, title: e.target.value })} />
         </div>
 
-        <div>
+        {/* <div>
           <label>Text</label>
           <textarea
             type="text"
             value={form.text}
             onChange={e => setForm({ ...form, text: e.target.value })} />
-        </div>
+        </div> */}
 
         <div className="buttons">
           <button classNAme="ripple" type="submit">Submit</button>
         </div>
       </form>
-
 
 
       {/* CARDS */}
@@ -77,18 +117,13 @@ export default function App() {
         {quotes.map((quote, index) => (
           <div className="cards" key={quote.id} >
             <p>{quote.title}</p>
-            <p dangerouslySetInnerHTML={{ __html: quote.text }}></p>
-
-            <div>
-              <button classNAme="ripple" className="remove" onClick={() => removeQuote(quote.id)}>Remove</button>
-            </div>
-
+            <p>{quote.text}</p>
+            <button classNAme="ripple" className="remove" onClick={() => removeQuote(quote.id)}>Remove</button>
           </div>
         ))
         }
 
         {/* {JSON.stringify(form)} */}
-
       </div >
     </div >
   );
